6f47322abe4a40c7851309a07fac1fb20cd3d44c
VladUsic <51832198+VladUsic@users.noreply.github.com>
2021-11-10 19:36:42 +0200

    first commit

---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,22 @@
+//import ContactForm from "../ContactForm"
+
+const ContactListItem = ({ id, name, phone, onRemove }) => {
+    return (
+        <li>
+            {name}: {phone} <button onClick={() => onRemove(id)}>delete</button>
+        </li>
+    )
+}
+
+const ContactsList = ({ contacts, onRemove }) => {
+    if(contacts.length === 0) return null
+    return (
+        <ul>
+            {contacts.map((contact) => (
+                <ContactListItem {...contact} onRemove={onRemove}/>
+            ))}
+        </ul>
+    )
+}
+
+export default ContactsList
